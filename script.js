c8edba959daa7785a2c2bead7b1bbf3043b6cb30
Niklausi <niko.mayr@hotmail.com>
2022-06-10 19:56:00 +0200

    added

---
+++ script.js
@@ -0,0 +1,4 @@
+var numb1 = "Hello world!";
+console.log(numb1);
+var numb2 = "Hello world!";
+console.log(numb2);
---
+++ script.ts
@@ -0,0 +1,5 @@
+let numb1 : string = "Hello world!";
+console.log(numb1);
+
+let numb2 : string = "Hello world!";
+console.log(numb2);
