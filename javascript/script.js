aa4445678ea36cb700af9f4cdc719d255f3fbfaa
Niklausi <niko.mayr@hotmail.com>
2022-06-20 19:24:23 +0200

    changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,83 @@
 "use strict";
 class Animal {
-    constructor(name, gender, height, age, vaccine) {
+    constructor(id, name, gender, height, age, vaccine, image) {
+        this.id = id;
         this.name = name;
         this.gender = gender;
         this.height = height;
         this.age = age;
         this.vaccine = vaccine;
+        this.image = image;
+        animalsArray.push(this);
     }
-    displayInfo() {
-        return `${this.name}, ${this.gender}, ${this.height}, ${this.age}, ${this.vaccine}`;
+    printCards() {
+        return `
+        <div class="card" style="width:250px">
+            <img class="card-img-top" src="${this.image}">
+            <div class="card-body">
+              <h4 class="card-title">Name:${this.name}</h4>
+              <p class="card-text">Gender ${this.gender}</p>
+              <p class="card-text">height ${this.height}</p>
+              <p class="card-text">Age:${this.age}</p>
+              <button id="${this.id}" onclick="vaccine(${this.id})" class="btn-vaccine" >Vaccine: ${this.vaccine}</button>
+            </div>
+          </div> 
+      </div>`;
     }
 }
+let animalsArray = [];
 class Cat extends Animal {
-    constructor(name, gender, height, age, vaccine, breed, furcolor, urlbreed) {
-        super(name, gender, height, age, vaccine);
+    constructor(id, name, gender, height, age, vaccine, image, breed, furcolor, urlbreed) {
+        super(id, name, gender, height, age, vaccine, image);
         this.breed = breed;
         this.furcolor = furcolor;
-        this.urlbreed = this.urlbreed;
+        this.urlbreed = urlbreed;
     }
-    displayInfo() {
-        return `${this.name}, ${this.gender}, ${this.height}, ${this.age}, ${this.vaccine}, ${this.breed}, ${this.furcolor}, ${this.urlbreed}`;
+    printCards() {
+        return `
+        <div class="card" style="width:250px">
+            <img class="card-img-top" src="${this.image}">
+            <div class="card-body">
+              <h4 class="card-title">Name:${this.name}</h4>
+              <p class="card-text">Gender ${this.gender}</p>
+              <p class="card-text">height ${this.height}</p>
+              <p class="card-text">Age:${this.age}</p>
+              <p class="card-text">Age:${this.breed}</p>
+              <p class="card-text">Age:${this.furcolor}</p>
+              <p class="card-text">Age:${this.urlbreed}</p>
+              <button id="${this.id}" onclick="vaccine(${this.id})" class="btn-vaccine" >Vaccine: ${this.vaccine}</button>
+            </div>
+          </div> 
+      </div>`;
     }
 }
 class Dog extends Animal {
-    constructor(name, gender, height, age, vaccine, breed, training) {
-        super(name, gender, height, age, vaccine);
+    constructor(id, name, gender, height, age, vaccine, image, breed, training) {
+        super(id, name, gender, height, age, vaccine, image);
         this.breed = breed;
         this.training = training;
     }
-    displayInfo() {
-        return `${this.name}, ${this.gender}, ${this.height}, ${this.age}, ${this.vaccine}, ${this.breed}, ${this.training}`;
+    printCards() {
+        return `
+        <div class="card" style="width:250px">
+            <img class="card-img-top" src="${this.image}">
+            <div class="card-body">
+              <h4 class="card-title">Name:${this.name}</h4>
+              <p class="card-text">Gender ${this.gender}</p>
+              <p class="card-text">height ${this.height}</p>
+              <p class="card-text">Age:${this.age}</p>
+              <button id="${this.id}" onclick="vaccine(${this.id})" class="btn-vaccine" >Vaccine: ${this.vaccine}</button>
+              <p class="card-text">Age:${this.breed}</p>
+              <p class="card-text">Age:${this.training}</p>
+            </div>
+          </div> 
+      </div>`;
     }
 }
+new Cat(1, "Minki", "female", "small", 1, "<i class='bi bi-shield-check'></i>", "cat1.jpg", "housecat", "whiteblack", "https://en.wikipedia.org/wiki/Cat");
+new Cat(2, "Mark", "male", "medium", 4, "<i class='bi bi-shield-check'></i>", "cat2.jpg", "wiskascat", "grey", "https://en.wikipedia.org/wiki/Cat");
+new Cat(3, "josh", "male", "large", 3, "<i class='bi bi-shield-fill-x'></i>", "coolcat.jpg", "coolcat", "darkgrey", "https://en.wikipedia.org/wiki/Cat");
+new Dog(1, "Rowdy", "male", "medium", 3, "<i class='bi bi-shield-fill-x'></i>", "dog1.jpg", "cooldog", false);
+new Dog(2, "Fred", "male", "small", 1, "<i class='bi bi-shield-check'></i>", "dog2.jpg", "housedog", true);
+new Dog(3, "Jasmin", "female", "large", 2, "<i class='bi bi-shield-fill-x'></i>", "dog3.jpg", "pug", false);
+let animalcont = document.getElementById("animalcont");
